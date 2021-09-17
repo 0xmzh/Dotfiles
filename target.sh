eab44c2cc1cf286ec86a7919961b935edaa458fa
M4zhh <89944287+M4zhh@users.noreply.github.com>
2021-09-17 17:51:17 -0500

    Update target.sh

--- target.sh
+++ target.sh
@@ -3,7 +3,7 @@
 target=$(cat ~/.config/bin/target)
  
 if [ "$target" = "" ]; then
-    echo "    什 OFF     "
+    echo "  %{F#960005}什 %{F#000000}OFF     "
 else
-    echo "什  $(cat ~/.config/bin/target)" 
+    echo "%{F#960005}什  %{F#000000}$(cat ~/.config/bin/target)" 
 fi
