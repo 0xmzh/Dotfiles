335e3be1c00714173d6e679e80ec0a8c25f5ad77
M4zhh <89944287+M4zhh@users.noreply.github.com>
2021-10-23 21:12:32 -0500

    Update target.sh

--- target.sh
+++ target.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
  
-target=$(cat ~/.config/bin/target)
+target=$(cat ~/.config/polybar/scripts/target)
  
 if [ "$target" = "" ]; then
-    echo "  %{F#960005}什 %{F#000000}OFF     "
+    echo "  %{F#960005}什 %{F#000000}Disconnected "
 else
-    echo "%{F#960005}什  %{F#000000}$(cat ~/.config/bin/target)" 
+    echo "%{F#960005}什  %{F#000000}$(cat ~/.configpolybar/scripts/target)" 
 fi
