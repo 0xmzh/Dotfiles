694932e28186baccb064d1d13e0841e44e537a13
+h2o <89944287+mazh2o@users.noreply.github.com>
2022-09-29 21:53:20 -0500

    Delete target.sh

--- target.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
- 
-target=$(cat ~/.config/polybar/scripts/target)
- 
-if [ "$target" = "" ]; then
-    echo "  %{F#960005}什 %{F#000000}Disconnected "
-else
-    echo "%{F#960005}什  %{F#000000}$(cat ~/.confi/polybar/scripts/target)" 
-fi
